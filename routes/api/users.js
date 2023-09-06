a8855e16a24b74e79032ec0402e6e97bfbf6f961
Michael Lee <funniest.dev730@gmail.com>
2023-09-06 13:30:01 -0700

    Implement user profile update function.

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-
+const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { ObjectId } = require('mongoose').Types;
 
 // @route    POST api/users
 // @desc     Register user
@@ -21,7 +22,6 @@ router.post(
   ).isLength({ min: 6 }),
   //check('birthday', 'Date of Birth is required').isDate(),
   async (req, res) => {
-    console.log(req.body)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -67,6 +67,70 @@ router.post(
           res.json({ token });
         }
       );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route    POST api/users
+// @desc     Update user
+// @access   Public
+router.post(
+  '/update', auth,
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'newPassword',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
+  check(
+    'confirmPassword',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
+  async (req, res) => {
+
+    userId = req.user.id;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, oldPassword, newPassword, confirmPassword } = req.body;
+
+    try {
+      let user = await User.findOne({ _id: new ObjectId(userId) });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid User!' }] });
+      }
+
+      if (email !== user.email) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Email!' }] });
+      }
+
+
+      if (oldPassword == newPassword) {
+        return res.status(400).json({ errors: [{ msg: 'New password is the same as old password!' }] });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      if (!bcrypt.compareSync(oldPassword, user.password)) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Password!' }] });
+      }
+
+      if (newPassword !== confirmPassword) {
+        return res.status(400).json({ errors: [{ msg: 'Passwords do not match!' }] });
+      }
+
+      user.password = await bcrypt.hash(newPassword, salt);
+      const result = await User.updateOne({ _id: new ObjectId(userId) }, { $set: user });
+
+      res.json(result)
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
