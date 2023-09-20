c51eb0bdafbb350c71dc94465b72d76a9bc1fa13
Michael Lee <funniest.dev730@gmail.com>
2023-09-20 09:10:31 -0700

    modify get all referrals.

--- controllers/referralController.js
+++ controllers/referralController.js
@@ -26,8 +26,60 @@ const setReferralLevel = async (req, res) => {
 }
 
 const getAllReferrals = async (req, res) => {
-    const referrals = await Referral.find();
-    res.status(200).json(referrals);
+
+    const page = parseInt(req.body.page) || 1;
+    const limit = parseInt(req.body.limit) || 10;
+
+    const count = await Referral.countDocuments();
+    console.log(count);
+    const totalPages = Math.ceil(count / limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < count) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        };
+    }
+
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        };
+    }
+
+    results.totalPages = totalPages;
+    results.results = await Referral.aggregate([{
+        $lookup: {
+            from: 'users',
+            localField: 'referralCode',
+            foreignField: 'myReferralCode',
+            as: 'userInfo',
+        }
+    },
+    { $unwind: '$userInfo' },
+    {
+        $project: {
+            _id: 1,
+            referralCode: 1,
+            level: 1,
+            invitesList: 1,
+            userId: 1,
+            firstName: '$userInfo.firstName',
+            lastName: '$userInfo.lastName',
+            email: '$userInfo.email'
+        },
+    },
+    { $skip: (page - 1) * limit }, // Skip documents based on the page number and limit
+    { $limit: limit },]);
+
+    // const referralsWithUserDetails = await User.populate(referrals, { path: 'userId', select: 'firstName lastName credits ETH_balance' });
+    res.status(200).json(results);
 }
 
 module.exports = { setReferralLevel, getAllReferrals };
