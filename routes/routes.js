830d4e76f34101e1c3035b8bd0396f07ba573652
Michael Lee <funniest.dev730@gmail.com>
2023-11-16 17:08:25 -0800

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -339,11 +339,11 @@ router.post('/admin/getfbsEventsG', geventController.getFBSEventsfromGoal);
 router.post('/props/update', propController.updateProps);
 
 
-// router.post('/pool/getmatchlist', auth, poolController.getMatchList);
-// router.post('/pool/betPool', auth, poolController.betPool);
-// router.post('/pool/checkPoolBet', auth, poolController.checkPoolBet);
-// router.post('/pool/getBetRes', auth, poolController.getBetRes);
-// router.post('/pool/testPool', auth, geventController.testPoolBets);
+router.post('/pool/getmatchlist', auth, poolController.getMatchList);
+router.post('/pool/betPool', auth, poolController.betPool);
+router.post('/pool/checkPoolBet', auth, poolController.checkPoolBet);
+router.post('/pool/getBetRes', auth, poolController.getBetRes);
+router.post('/pool/testPool', auth, geventController.testPoolBets);
 router.post('/admin/getmmaEventsG', geventController.getMMAEventsfromGoal);
 
 router.post('/admin/updatePlayerSportType', playerController.updateIndividualPlayer);
